c6403ab50b472957f6916e4ee03a59632a8481b3
Daniel <danielisaacjuarez@gmail.com>
2023-12-14 00:27:46 -0600

    video 3 done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,36 @@
 import react from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import {colors} from '../constants/theme';
 import MainHeader from '../components/MainHeader';
-
+import ScreenHeader from '../components/ScreenHeader';
+import TopPlacesCarousel from '../components/TopPlacesCarousel';
+import { PLACES, TOP_PLACES } from "../data/index";
+import SectionHeader from '../components/SectionHeader';
+import TripsList from '../components/TripsList';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <MainHeader title='Home'/>
+      <MainHeader title="Travel App" />
+      <ScreenHeader mainTitle="Find Your" secondTitle="Dream Trip" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <TopPlacesCarousel list={TOP_PLACES} />
+        <SectionHeader
+          title="Popular Trips"
+          buttonTitle="See All"
+          onPress={() => {}}
+        />
+        <TripsList list={PLACES} />
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.light,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: colors.light,
+  },
+});
 
 export default HomeScreen;
